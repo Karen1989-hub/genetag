6783f140b39af1ddc239545275e629e0135b0ef6
Azat <you@example.com>
2024-03-04 14:21:48 +0400

    taza ban

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,13 +1,4 @@
 Yerevan
-new porc 22
-br1 docs
-<h1>New branch 123456</h1>
-<p>nor tarmacumner </p>
-<p> Norutyun Armanic</p>
-<<<<<<< HEAD
-<p>norutyun Azatic 33</p>
-=======
-<p>popoxutyun armna brenchum
-</p>
->>>>>>> 7edab1bf7de496af27e7f7968de47149506032c6
-e2
+
+taza ban
+
